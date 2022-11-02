e5e166f98ab7df34ab784a2457ae906f959d5cd1
yves <jeanpetityvelos@gmail.com>
2022-11-02 10:33:40 +0100

    customize errorhandler

--- backend/src/api.py
+++ backend/src/api.py
@@ -71,7 +71,37 @@ def create_app(test_config=None):
         except:
             abort(404)
         
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "ressource Not found"
+        }), 404
+    
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad request"
+        }), 400
         
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+        }), 422
+    
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'method not allowed'
+        })
         
     return app
     
